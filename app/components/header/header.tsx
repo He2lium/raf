f70f873f330669c58d1e6dbed0c64409b34f7d7e
Dmitrii <stepanov.dmitrii.spo@gmail.com>
2024-11-15 14:09:30 +0400

    pravki 2

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,18 +1,46 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import styles from './header.module.css';
 
 export const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const menuRef = useRef(null);
 
 	const toggleMenu = () => {
 		setIsOpen(!isOpen);
 		document.body.classList.toggle('opened');
 	};
+
+	const handleMenuItemClick = () => {
+		setIsOpen(false);
+		document.body.classList.remove('opened');
+	};
+
+	const handleClickOutside = useCallback((event: MouseEvent) => {
+		if (
+			menuRef.current &&
+			!(menuRef.current as HTMLElement).contains(event.target as Node)
+		) {
+			handleMenuItemClick();
+		}
+	}, []);
+
+	useEffect(() => {
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen, handleClickOutside]);
+
 	return (
-		<header className={`${styles['header-wrapper']} header`}>
+		<header className={`${styles['header-wrapper']} header`} ref={menuRef}>
 			<div className='container'>
 				<div className={`${styles.header}`}>
 					<nav className={styles.navbar}>
@@ -22,32 +50,56 @@ export const Header = () => {
 
 						<ul className={`${styles.navmenu} ${isOpen ? styles.active : ''}`}>
 							<li className={styles.navitem}>
-								<Link href='#!' className={styles.navlink}>
+								<Link
+									href='#!'
+									className={styles.navlink}
+									onClick={handleMenuItemClick}
+								>
 									Кому подойдет?
 								</Link>
 							</li>
 							<li className={styles.navitem}>
-								<Link href='#!' className={styles.navlink}>
+								<Link
+									href='#!'
+									className={styles.navlink}
+									onClick={handleMenuItemClick}
+								>
 									Легкий старт
 								</Link>
 							</li>
 							<li className={styles.navitem}>
-								<Link href='#!' className={styles.navlink}>
+								<Link
+									href='#!'
+									className={styles.navlink}
+									onClick={handleMenuItemClick}
+								>
 									Вопрос - ответ
 								</Link>
 							</li>
 							<li className={styles.navitem}>
-								<Link href='#!' className={styles.navlink}>
+								<Link
+									href='#!'
+									className={styles.navlink}
+									onClick={handleMenuItemClick}
+								>
 									Программа курса
 								</Link>
 							</li>
 							<li className={styles.navitem}>
-								<Link href='#!' className={styles.navlink}>
+								<Link
+									href='#!'
+									className={styles.navlink}
+									onClick={handleMenuItemClick}
+								>
 									Тарифы
 								</Link>
 							</li>
 							<li className={styles.navitem}>
-								<Link href='#!' className={styles.navlink}>
+								<Link
+									href='#!'
+									className={styles.navlink}
+									onClick={handleMenuItemClick}
+								>
 									Контакты
 								</Link>
 							</li>
